dce5362ea1522962fa3bf4c3c25a1f105e3ba1fa
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2020-01-07 02:28:37 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace C_Sharp_Number_Analysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi, birlerB, onlarB, yuzlerB, binlerB;
+            Console.Write("Bir sayı giriniz: ");
+            sayi = int.Parse(Console.ReadLine());
+
+            string[] birler = { "", "bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz" };
+            string[] onlar = { "", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan" };
+            string[] yuzler = { "", "yüz", "ikiyüz", "üçyüz", "dörtyüz", "beşyüz", "altıyüz", "yediyüz", "sekizyüz", "dokuzyüz" };
+            string[] binler = { "", "bin", "ikibin", "üçbin", "dörtbin", "beşbin", "altıbin", "yedibin", "sekizbin", "dokuzbin" };
+
+            birlerB = sayi % 10;
+            onlarB = (sayi / 10) % 10;
+            yuzlerB = (sayi / 100) % 10;
+            binlerB = (sayi / 1000) % 10;
+
+            Console.WriteLine(binler[binlerB] + " " + yuzler[yuzlerB] + " " + onlar[onlarB] + " " + birler[birlerB]);
+
+            Console.Read();
+        }
+    }
+}
